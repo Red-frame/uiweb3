80295a2f29950caf6fa56d01178842810ad2fb53
Red-frame <135748645+Red-frame@users.noreply.github.com>
2026-01-23 05:03:53 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import ChatBox from "../components/ChatBox";
+
 export default function Home() {
   return (
     <div className="flex flex-col h-dvh max-w-md mx-auto bg-white">
