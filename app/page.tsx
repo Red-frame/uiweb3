04f2b5b62baa439a81c846c0bb172152f8f428ba
Red-frame <135748645+Red-frame@users.noreply.github.com>
2026-01-23 05:08:21 +0700

    Update import paths for Navbar and ChatBox components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Navbar from "../components/Navbar";
-import ChatBox from "../components/ChatBox";
+import Navbar from "./components/Navbar";
+import ChatBox from "./components/ChatBox";
 
 export default function Home() {
   return (
